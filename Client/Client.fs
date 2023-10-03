53f2d05423fe19594d9a84cf5a25e892a3c72a92
Girish Salunke <girishsalunke@Girishs-MacBook-Air.local>
2023-10-03 15:40:51 -0400

    Basic Client Server Functionality

--- Client/Client.fs
+++ Client/Client.fs
@@ -1,2 +1,37 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from Client"
+﻿// Import Statements
+open System
+open System.Net.Sockets
+open System.IO
+
+// Define Socket Address (Server IP Address and Port Number)
+let serverAddress = "127.0.0.1"
+let serverPort = 8000
+
+// Define the Client Function to handle Client Functionality
+let client () =
+    try
+        let client = new TcpClient(serverAddress, serverPort)
+        let stream = client.GetStream()
+        let reader = new StreamReader(stream)
+        let writer = new StreamWriter(stream)
+
+        let rec sendReceive () =
+            printf "Enter a message (or 'exit' to quit): "
+            let message = Console.ReadLine()
+            if message.ToLower() = "exit" then
+                client.Close()
+            else
+                writer.WriteLine(message)
+                writer.Flush()
+                let data = reader.ReadLine()
+                printfn "Received: %s" data
+                sendReceive()
+
+        sendReceive()
+    with
+    | ex -> printfn "Error: %s" ex.Message
+
+[<EntryPoint>]
+let main argv =
+    client()
+    0
--- Server/Server.fs
+++ Server/Server.fs
@@ -1,2 +1,41 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from Server"
+﻿open System
+open System.Net
+open System.Net.Sockets
+open System.IO
+
+let serverPort = 8000
+
+let server () =
+    let ipAddress = IPAddress.Parse("127.0.0.1")
+    let listener = new TcpListener(ipAddress, serverPort)
+    listener.Start()
+    printfn "Server listening on port %d" serverPort
+
+    let rec handleClientAsync (client : TcpClient) =
+        let stream = client.GetStream()
+        let reader = new StreamReader(stream)
+        let writer = new StreamWriter(stream)
+        async {
+            while true do
+                let data = reader.ReadLine()
+                printfn "Received: %s" data
+                writer.WriteLine("Server: " + data)
+                writer.Flush()
+        }
+
+    let rec acceptClients () =
+        let client = listener.AcceptTcpClient()
+        printfn "Client connected"
+        let clientTask = async {
+            do! handleClientAsync client
+        }
+        Async.Start clientTask
+        acceptClients ()
+
+    acceptClients ()
+
+[<EntryPoint>]
+let main argv =
+    server()
+    Console.ReadKey() |> ignore
+    0
